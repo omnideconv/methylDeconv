4ed2e41d51b4608d1879072d1f71b7defa5ee311
puerckhauer <puerckhauer@cip.ifi.lmu.de>
2023-04-09 15:52:16 +0200

    name update

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(FoPraPackage)
+library(MeDeUI)
 
-test_check("FoPraPackage")
+test_check("MeDeUI")
